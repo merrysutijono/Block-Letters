0516ea41b5bcf3caca2478e98651d1d0ae43bf62
merrysutijono <68658448+merrysutijono@users.noreply.github.com>
2023-08-05 21:09:23 -0700

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,14 @@
+// Fun Fact: I love binge-watching TV shows!
+#include <iostream>
+
+int main ()
+{
+  // Your program lives here:
+  std::cout <<"M     M   SSS\n";
+  std::cout <<"MM   MM  S   S\n";
+  std::cout <<"MM   MM  S\n";
+  std::cout <<"M  M  M   SSS\n";  
+  std::cout <<"M     M      S\n";
+  std::cout <<"M     M  S   S\n";
+  std::cout <<"M     M   SSS\n";
+}
